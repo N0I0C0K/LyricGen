e9351b88449052f711884f9140cd8d5f8da3c2f7
N0I0C0K <nick131410@aliyun.com>
2023-12-30 14:27:45 +0800

    feat：添加了激活层

--- model.py
+++ model.py
@@ -14,7 +14,8 @@ class SongNet(torch.nn.Module):
         self.lstm = nn.LSTM(
             embedding_size, lstm_hidden_size, num_layers=2, batch_first=True
         )
-        # self.h2h = nn.Linear(lstm_hidden_size, lstm_hidden_size)
+        self.h2h = nn.Linear(lstm_hidden_size, lstm_hidden_size)
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.01)
         self.h2o = nn.Linear(lstm_hidden_size, num_embeddings)
 
     def forward(self, raw, hidden_state=None, noise: bool = False):
@@ -24,7 +25,7 @@ class SongNet(torch.nn.Module):
 
         out, hidden_state = self.lstm(embedd, hidden_state)
 
-        # out = self.h2h(out)
+        out = self.leaky_relu(self.h2h(out))
 
         out = self.h2o(out)
 
